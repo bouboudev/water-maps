fd61017b22ad9e30916016515015f9a8c28f5014
Bouzid Krita <bouzid.krita@hotmail.fr>
2022-03-19 12:14:57 +0100

    fix method valid with validate

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -6,79 +6,86 @@
           Ajouter un forage
         </v-btn>
       </template>
+        <v-form ref="form">
       <v-card>
-        <v-card-title>
-          <span class="text-h5">Ajouter un forage</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  label="Nom du forage"
-                  v-model="nameDrilling"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  label="Nom du village"
-                  v-model="nameVillage"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  label="Latitude"
-                  v-model="latitudeDrilling"
-                  hint="Exemple: 48.887737638046794"
-                ></v-text-field>
-                <v-text-field
-                  label="Longitude"
-                  v-model="longitudeDrilling"
-                  hint="Exemple: 48.887737638046794"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Image" v-model="imageUrl"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-menu
-                  v-model="menu"
-                  :close-on-content-click="false"
-                  :nudge-right="40"
-                  transition="scale-transition"
-                  offset-y
-                  min-width="auto"
-                >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-text-field
-                      v-model="formattedDate"
-                      label="Picker without buttons"
-                      prepend-icon="mdi-calendar"
-                      readonly
-                      v-bind="attrs"
-                      v-on="on"
-                    ></v-text-field>
-                  </template>
-                  <v-date-picker
-                    v-model="datePicker"
-                    @input="menu = false"
-                  ></v-date-picker>
-                </v-menu>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small class="red--text">*Champs obligatoire</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="dialog = false">
-            Annuler
-          </v-btn>
-          <v-btn color="blue darken-1" text @click="valid"> Ajouter </v-btn>
-        </v-card-actions>
+          <v-card-title>
+            <span class="text-h5">Ajouter un forage</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Nom du forage"
+                    v-model="nameDrilling"
+                    :rules="inputRules"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Nom du village"
+                    v-model="nameVillage"
+                    :rules="inputRules"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Latitude"
+                    v-model="latitudeDrilling"
+                    hint="Exemple: 48.887737638046794"
+                    :rules="inputRules"
+                  ></v-text-field>
+                  <v-text-field
+                    label="Longitude"
+                    v-model="longitudeDrilling"
+                    hint="Exemple: 48.887737638046794"
+                    :rules="inputRules"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field label="Image" v-model="imageUrl"></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-menu
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="auto"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                        v-model="formattedDate"
+                        label="Picker without buttons"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                        :rules="inputRules"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker
+                      v-model="datePicker"
+                      @input="menu = false"
+                    ></v-date-picker>
+                  </v-menu>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small class="red--text">*Champs obligatoire</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Annuler
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="valid"> Ajouter </v-btn>
+          </v-card-actions>
       </v-card>
+        </v-form>
     </v-dialog>
   </v-row>
 </template>
@@ -99,12 +106,14 @@ export default {
       datePicker: null,
       menu: false,
       modal: false,
+      inputRules: [(v) => v.length >= 3 || "Le Minimum est de 3 caractères."],
     };
   },
   methods: {
     valid() {
-      this.dialog = false;
-      console.log(this.nameDrilling, this.nameVillage);
+      if (this.$refs.form.validate()) {
+        console.log("ça mache !!!");
+      }
     },
   },
   computed: {
