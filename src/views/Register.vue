fab15eaeb7eda89b74a76c69234d53ed05a47e5a
Bouzid Krita <bouzid.krita@hotmail.fr>
2022-03-29 18:08:54 +0200

    ajout de la confirmation du mot de passe

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -12,7 +12,13 @@
           {{ error }}
 
           <template v-slot:action="{ attrs }">
-            <v-btn color="pink" text v-bind="attrs" @click="snackbar = false" :timeout="3000">
+            <v-btn
+              color="pink"
+              text
+              v-bind="attrs"
+              @click="snackbar = false"
+              :timeout="3000"
+            >
               Fermer
             </v-btn>
           </template>
@@ -35,82 +41,97 @@
       <h1>Rejoins-nous !</h1>
 
       <h3 class="mt-8 mb-5 white--text">S'inscrire</h3>
-      <v-form
-    ref="form"
-    v-model="isvalid"
-    lazy-validation
-    color="grey"
-  > 
-      <v-text-field
-        filled
-        background-color="white"
-        color="grey-dark"
-        label="Nom de l'association"  
-        :rules="caractereRules"
-        required
-        v-model="assosRegister"
-      ></v-text-field>
-      <v-text-field
-        filled
-        background-color="white"
-        color="grey-dark"
-        label="Prénom"
-        :rules="caractereRules"
-        required
-        v-model="firstNameRegister"
-      ></v-text-field>
-      <v-text-field
-        filled
-        background-color="white"
-        color="grey-dark"
-        label="Nom"
-        :rules="caractereRules"
-        required
-        v-model="lastNameRegister"
-      ></v-text-field>
-      <v-text-field
-        filled
-        background-color="white"
-        color="grey-dark"
-        label="Numéro de télépone"
-        required
-        :counter="10"
-        :rules="numberRules"
-        error-count="3"
-        v-model="numberRegister"
-      ></v-text-field>
-      <v-text-field
-        filled
-        background-color="white"
-        color="grey-dark"
-        label="Adresse mail"
-        required
-        :rules="caractereRules"
-        v-model="emailRegister"
-      ></v-text-field>
-      <v-text-field
-        filled
-        background-color="white"
-        color="grey-dark"
-        :append-icon="showRegister ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="showRegister ? 'text' : 'password'"
-        name="input-10-2"
-        label="Mot de passe"
-        @click:append="showRegister = !showRegister"
-        required
-        :rules="passwordRules"
-        error-count="5"
-        v-model="passwordRegister"
-      ></v-text-field>
+      <v-form ref="form" v-model="isvalid" lazy-validation color="grey">
+        <v-text-field
+          filled
+          background-color="white"
+          color="grey-dark"
+          label="Nom de l'association"
+          :rules="caractereRules"
+          required
+          v-model="assosRegister"
+        ></v-text-field>
+        <v-text-field
+          filled
+          background-color="white"
+          color="grey-dark"
+          label="Prénom"
+          :rules="caractereRules"
+          required
+          v-model="firstNameRegister"
+        ></v-text-field>
+        <v-text-field
+          filled
+          background-color="white"
+          color="grey-dark"
+          label="Nom"
+          :rules="caractereRules"
+          required
+          v-model="lastNameRegister"
+        ></v-text-field>
+        <v-text-field
+          filled
+          background-color="white"
+          color="grey-dark"
+          label="Numéro de télépone"
+          required
+          :counter="10"
+          :rules="numberRules"
+          error-count="3"
+          v-model="numberRegister"
+        ></v-text-field>
+        <v-text-field
+          filled
+          background-color="white"
+          color="grey-dark"
+          label="Adresse mail"
+          required
+          :rules="caractereRules"
+          v-model="emailRegister"
+        ></v-text-field>
+        <v-text-field
+          filled
+          background-color="white"
+          color="grey-dark"
+          :append-icon="showRegister ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="showRegister ? 'text' : 'password'"
+          name="input-10-2"
+          label="Mot de passe"
+          @click:append="showRegister = !showRegister"
+          required
+          :rules="passwordRules"
+          error-count="5"
+          v-model="passwordRegister"
+        ></v-text-field>
+        <v-text-field
+          filled
+          background-color="white"
+          color="grey-dark"
+          :append-icon="showRegister ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="showRegister ? 'text' : 'password'"
+          name="input-10-2"
+          label="Confirmer le mot de passe"
+          @click:append="showRegister = !showRegister"
+          required
+          :rules="confirmPasswordRules"
+          error-count="5"
+          v-model="confirmPasswordRegister"
+        ></v-text-field>
 
-      <v-alert type="error" v-show="error != ''"> {{ error }}</v-alert>
-      <div>
-        <v-btn small text @click="haveAccount">Déjà un compte ?</v-btn>
-      </div>
+        <v-alert type="error" v-show="error != ''"> {{ error }}</v-alert>
+        <div>
+          <v-btn small text @click="haveAccount">Déjà un compte ?</v-btn>
+        </div>
 
-      <v-btn @click="register" :disabled="!isvalid" class="mt-4" light color="primary--text" >
-        S'inscrire
-      </v-btn>
+        <v-btn
+          @click="register"
+          :disabled="!isvalid"
+          class="mt-4"
+          light
+          color="primary--text"
+        >
+          S'inscrire
+        </v-btn>
       </v-form>
     </v-col>
   </v-row>
@@ -119,7 +140,6 @@
 <script>
 import firebase from "firebase";
 import db from "@/main";
-
 
 export default {
   name: "register",
@@ -136,35 +156,46 @@ export default {
       numberRegister: "",
       emailRegister: "",
       passwordRegister: "",
+      confirmPasswordRegister: "",
       error: "",
       message: "",
       isvalid: true,
       caractereRules: [
-        v => !!v || 'le champs est recquis ',
-        v => (v && v.length >= 3) || 'Minimum de 3 caractères',
+        (v) => !!v || "le champs est recquis ",
+        (v) => (v && v.length >= 3) || "Minimum de 3 caractères",
       ],
-       numberRules: [
-        v => !!v || 'Le numéro est recquis',
-        v => (v && v.length >= 10) || 'Le numéro doit être de 10 chiffres',
-        
+      numberRules: [
+        (v) => !!v || "Le numéro est recquis",
+        (v) => (v && v.length >= 10) || "Le numéro doit être de 10 chiffres",
       ],
       emailRules: [
-        v => !!v || 'E-mail est recquis',
-        v => /.+@.+\..+/.test(v) || 'E-mail doit être valide',
+        (v) => !!v || "E-mail est recquis",
+        (v) => /.+@.+\..+/.test(v) || "E-mail doit être valide",
       ],
-      passwordRules: [ 
-    v => !!v || 'le mot de passe est recquis', 
-    v => (v && v.length >= 5) || 'le mot de passe doit avoir au minimum 5 caractères',
-    v => /(?=.*[A-Z])/.test(v) || 'le mot de passe doit avoir une Majuscule', 
-    v => /(?=.*\d)/.test(v) || 'le mot de passe doit avoir un numéro', 
-    v => /([!@$%])/.test(v) || 'le mot de passe doit avoir un caracère spéciale : [!@#$%]' 
-]
+      passwordRules: [
+        (v) => !!v || "le mot de passe est recquis",
+        (v) =>
+          (v && v.length >= 5) ||
+          "le mot de passe doit avoir au minimum 5 caractères",
+        (v) =>
+          /(?=.*[A-Z])/.test(v) || "le mot de passe doit avoir une Majuscule",
+        (v) => /(?=.*\d)/.test(v) || "le mot de passe doit avoir un numéro",
+        (v) =>
+          /([!@$%])/.test(v) ||
+          "le mot de passe doit avoir un caracère spéciale : [!@#$%]",
+      ],
+      confirmPasswordRules: [
+        (v) => !!v || "la confirmation du mot de passe est recquis",
+        (v) =>
+          v === this.passwordRegister ||
+          "la confirmation du mot de passe doit être identique au mot de passe",
+      ],
     };
   },
   methods: {
     register() {
-        this.$refs.form.validate()
-   
+      this.$refs.form.validate();
+
       firebase
         .auth()
         .createUserWithEmailAndPassword(
@@ -205,7 +236,6 @@ export default {
 };
 </script>
 <style scoped>
-
 .register {
   height: 100%;
 }
